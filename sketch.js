2e919faf07e49a72a11939977e69d9c75327050b
Artem <artemkotovno@proton.me>
2023-11-01 14:24:06 +0100

    Two circles moving at different speeds, changes direction when wall hit

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
 let circleX, circleY, circleSize;
 let circle2X, circle2Y;
+let direction = 3;
+let direction2 = -1;
 
 function setup() {
   createCanvas(500, 500);
   circleSize = 50;
   circleX = circleSize / 2;
   circleY = height / 6;
-  circle2X = circleSize / 2;
+  circle2X = width - circleSize / 2;
   circle2Y = height / 1.25;
 }
 
@@ -14,4 +16,21 @@ function draw() {
   background(220);
   circle(circleX, circleY, circleSize); 
   circle(circle2X, circle2Y, circleSize);
-}
+  
+  circleX += 2 * direction;
+    if (circleX >= width - circleSize / 2) {
+    direction = -3;
+  }
+    if (circleX <= circleSize / 2) {
+    direction = 3;
+  }
+
+  circle2X += 2 * direction2;
+  if (circle2X >= width - circleSize / 2) {
+    direction2 = -1;
+  }
+  if (circle2X <= circleSize / 2) {
+    direction2 = 1;
+  }
+  }
+
