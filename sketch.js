c4ce0882ff1498c0f9e1c68488993a5ad829884e
Artem <artemkotovno@proton.me>
2023-11-01 14:09:40 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
+let circleX, circleY, circleSize;
+let circle2X, circle2Y;
+
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(500, 500);
+  circleSize = 50;
+  circleX = circleSize / 2;
+  circleY = height / 6;
+  circle2X = circleSize / 2;
+  circle2Y = height / 1.25;
 }
 
 function draw() {
   background(220);
+  circle(circleX, circleY, circleSize); 
+  circle(circle2X, circle2Y, circleSize);
 }
