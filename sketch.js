13770970624f9a519069425195d64972859167ff
Artem <artemkotovno@proton.me>
2023-11-01 13:22:01 +0100

    colorrgb circle spawn

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let circles = [];
 let circleSize = 50;
+let isDrawing = false;
 
 function setup() {
   createCanvas(400, 400);
@@ -12,18 +13,20 @@ function draw() {
     let d = dist(mouseX, mouseY, circles[i].x, circles[i].y);
 
     if (d < circleSize / 2) {
-      circles[i].color = color(255, 0, 0);
+      circles[i].color = color(255, 0, 0,);
     } else {
-      circles[i].color = color(255);
+      // Add a flowing RGB effect to the circles when they are not hovered over
+      let r = sin(frameCount * 0.05 + i * 0.1) * 127 + 128;
+      let g = sin(frameCount * 0.05 + i * 0.2) * 127 + 128;
+      let b = sin(frameCount * 0.05 + i * 0.3) * 127 + 128;
+      circles[i].color = color(r, g, b);
     }
 
     fill(circles[i].color);
     circle(circles[i].x, circles[i].y, circleSize);
   }
-}
-
-function mousePressed() {
-  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+  
+  if (isDrawing) {
     let newCircle = {
       x: mouseX,
       y: mouseY,
@@ -32,3 +35,30 @@ function mousePressed() {
     circles.push(newCircle);
   }
 }
+
+function mousePressed() {
+  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    isDrawing = true;
+  }
+}
+
+function mouseReleased() {
+  isDrawing = false;
+}
+
+function getRandomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function changeCircleColor() {
+  for (let i = 0; i < circles.length; i++) {
+    circles[i].color = color(getRandomColor());
+  }
+}
+
+setInterval(changeCircleColor, 1000);
