3f3665f5dbc6f957bd0c9acc022b6aca72ad3c9a
Artem <artemkotovno@proton.me>
2023-11-01 14:04:51 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,18 +4,4 @@ function setup() {
 
 function draw() {
   background(220);
-  fill(255,0,0)
-  circle(62, 300, 125)
-
-  fill(0,255,0)
-  circle(187, 300, 125)
-
-  fill(0,0,255)
-  circle(312, 300, 125)
-
-  fill(255,255,0)
-  circle(437, 300, 125)
-
-  fill(255,0,255)
-  circle(562, 300, 125)
 }
