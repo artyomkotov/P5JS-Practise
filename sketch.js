2c9c9616dec868bd4a1eeda22400ddaeeb20279e
Artem <artemkotovno@proton.me>
2024-01-03 14:39:48 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,15 +3,16 @@ let speedX, speedY;
 let player1scr, player2scr;
 circleX = 350;
 circleY = 250;
-player1Y = 50;
-player2Y = 50;
-
+player1Y = 200;
+player2Y = 200;
+point1 = 0;
+point2 = 0;
 
 function setup() {
   createCanvas(700, 500);
 
-  speedX = random(-5, 5);
-  speedY = random(-5, 5);
+  speedX = random([-5, 5]);
+  speedY = random([-5, 5]);
 
   player2X = width - 25;
   player1X = 15;
@@ -19,10 +20,35 @@ function setup() {
 }
 
 function draw() {
-  background(100);
+  background(0);
+  fill( 109, 4, 137);
   circle(circleX, circleY, 50);
   circleX = circleX + speedX;
   circleY = circleY + speedY;
+
+function resetBall() {
+  circleX = 350
+  circleY = 250
+
+  speedX = random([-5, 5]);
+  speedY = random([-5, 5]);
+}
+
+
+  if (circleX > width + 25) {
+    point1 = point1 + 1;
+    resetBall();
+  }
+
+  if (circleX < -25) {
+    point2 = point2 + 1;
+    resetBall();
+  }
+
+  fill(255, 255, 255);
+  textSize(32);
+  text(point2, width/2 + 100, 50);  
+  text(point1, width/2 - 100, 50);
 
   if (circleY < 25) {
       speedY = speedY * (-1);
@@ -31,7 +57,7 @@ function draw() {
   if (circleY + 25 > height) {
       speedY = speedY * (-1);
       }
-  
+  fill(255, 0, 0);
   rect(player1X, player1Y, 10, 100);
   
   if (keyIsDown(83)) {
@@ -41,7 +67,7 @@ function draw() {
   if (keyIsDown(87)) {
     player1Y = player1Y - 10;
   }
-
+fill(0, 0, 255);
   rect(player2X, player2Y, 10, 100)
   
   if (keyIsDown(DOWN_ARROW)) {
@@ -59,14 +85,16 @@ function draw() {
   // check if the ball hits the right paddle
   if (circleX + 25 > player2X ) {
     if (circleY > player2Y && circleY < player2Y + 100) {
-      speedX = speedX * (-1);
+      speedX = (speedX + 0.25) * (-1);
+      circleX = player2X - 30;
     }
   }  
 
   // check if the ball hits the left paddle
   if (circleX - 25 < player1X + 10) {
     if (circleY > player1Y && circleY < player1Y + 100) {
-      speedX = speedX * (-1);
+      speedX = (speedX - 0.25) * (-1);
+      circleX = player1X + 30;
     }
   }
 }
